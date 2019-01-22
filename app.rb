cc1cd729d39b68fa95b536e51df15559d343150d
Samkttn <kettani.samir@outlook.fr>
2019-01-22 15:34:01 +0100

    Save dans mail.json

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require 'bundler'
+require 'json'
 Bundler.require
 
 $:.unshift File.expand_path('./../lib', __FILE__)
+require "scrapper"
+scrapper = ScrapperMail.new
+scrapper.save_as_JSON
+
--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -4,34 +4,32 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'json'
 
-class Scrapper_Mail
-    attr_accessor :mail_list, :city_names, :mail
+class ScrapperMail
     
-def get_mail
-  @city_names = []
-  @mail = []
+    
+def initialize
+  city_names = []
+  mail = []
   page = Nokogiri::HTML(open('http://annuaire-des-mairies.com/val-d-oise.html'))
 
   page.xpath('//a[@class="lientxt"]').each do |row|
     city_names.push(row.text)
     mail.push(Nokogiri::HTML(open('http://annuaire-des-mairies.com' + row['href'].sub('.', ''))).xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text)
   end
-  mail_list = city_names.zip(mail)
+  @mail_list = city_names.zip(mail).to_h
   
-  return  @mail_list
+  
 end
 
-def save_as_JSON(mail_list)
-    mail_list.to_h
-    file =File.open("../scrapper_AuSa/db/mail.json","w") do |f|
-  f.write(mail_list.to_json)
-    end
+def save_as_JSON
+    file = File.open("db/mail.json","w")
+    file.write(@mail_list.to_json)
+    file.close
 end
+
 #get_mail
-save_as_JSON(mail_list)
-end
-
 #binding.pry
 #puts "end of fil"
-
+end
